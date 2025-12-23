c5e1cc052b834a8bc91f148bbc49bece353340e2
Reed Martz <reed@reedmartz.com>
2025-12-23 17:20:41 -0500

    Convert project to TS

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import type { Express, Request, Response } from "express";
+import express from 'express';
+
+const app: Express = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
