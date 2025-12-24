f9bea8e1c3e6abbe3a16149aafbeea399f486cd9
Reed Martz <reed@reedmartz.com>
2025-12-24 14:26:39 -0500

    Add stub test framework and step in GH actions

---
+++ src/server/tests/api.test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+describe("stub test section", () => {
+  it('should succeed', () => {
+    assert.strictEqual(true, true);
+  });
+});
